19e27d170d50169722d422c622023a84cb60990c
Predrag Milicevic <budjenje2000@gmail.com>
2024-03-15 00:32:47 +0100

    Stream video context provider
    setting up video client context api

---
+++ src/app/error.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function ErrorPage() {
+  return (
+    <div className="space-y-3 text-center">
+      <h1 className="text-2xl font-bold">Error</h1>
+      <p>
+        Something went wrong with the streaming client. Please try again later.
+      </p>
+    </div>
+  );
+}
