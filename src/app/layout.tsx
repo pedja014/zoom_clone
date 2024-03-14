72a61852fbb1588a28652ea42377986e300d2347
Predrag Milicevic <budjenje2000@gmail.com>
2024-03-14 23:48:38 +0100

    Login / Logout Clerk user button
    setting up clerk functionality for signed in/out users

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Navbar />
+          <main className="max-w-5xl mx-auto px-3 py-3">{children}</main>
+        </body>
       </html>
     </ClerkProvider>
   );
