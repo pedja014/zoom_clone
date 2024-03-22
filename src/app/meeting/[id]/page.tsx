8b0c67016a3249cf082c1d3a537ae9ed83711abc
Predrag Milicevic <budjenje2000@gmail.com>
2024-03-22 21:37:45 +0100

    Meeting Loading Page

--- src/app/meeting/[id]/page.tsx
+++ src/app/meeting/[id]/page.tsx
@@ -1,8 +1,11 @@
 import { Metadata } from "next";
 import MeetingPage from "./MeetingPage";
+import { currentUser } from "@clerk/nextjs/server";
+import MeetingLoginPage from "./MeetingLoginPage";
 
 interface PageProps {
   params: { id: string };
+  searchParams: { guest: string };
 }
 
 export function generateMetadata({ params: { id } }: PageProps): Metadata {
@@ -11,6 +14,17 @@ export function generateMetadata({ params: { id } }: PageProps): Metadata {
   };
 }
 
-export default function Page({ params: { id } }: PageProps) {
+export default async function Page({
+  params: { id },
+  searchParams: { guest },
+}: PageProps) {
+  const user = await currentUser();
+
+  const guestMode = guest === "true";
+
+  if (!user && !guestMode) {
+    return <MeetingLoginPage />;
+  }
+
   return <MeetingPage id={id} />;
 }
